57e46d1586ad436213153d14e126b30c0ba96ca4
Deight <d8corp@mail.ru>
2026-04-18 21:55:02 +0300

    v1.0.3

--- release/_virtual/_rollup-plugin-process-env.mjs
+++ release/_virtual/_rollup-plugin-process-env.mjs
@@ -1,5 +1,5 @@
 ;(function () {
-  const env = {"__MCP__PACKAGE_VERSION":"1.0.2"};
+  const env = {"__MCP__PACKAGE_VERSION":"1.0.3"};
   if (typeof process === 'undefined') {
     globalThis.process = { env: env };
   } else if (process.env) {
